ca6af6c9e6cf9500d9d9fc2a23357ffc7f2f6718
Radek Zahradník <kutlime@gmail.com>
2023-02-12 20:53:37 +0100

    Add delegate command implementation

--- src/Template App/GlobalUsings.cs
+++ src/Template App/GlobalUsings.cs
@@ -3,4 +3,5 @@ global using System.Collections;
 global using System.ComponentModel;
 global using System.Runtime.CompilerServices;
 global using System.Windows;
+global using System.Windows.Input;
 global using TemplateApp.Extensions;
