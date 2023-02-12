ed24cb9043beaf2b2ad30e2eb3caf501a2c332f3
Radek Zahradník <kutlime@gmail.com>
2023-02-12 20:18:31 +0100

    Add DI extension method for configuration

---
+++ src/Template App/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace TemplateApp.Extensions;
+
+internal static class ServiceProviderExtensions
+{
+    internal static IServiceCollection ConfigureServices(this IServiceCollection services) =>
+        services.AddTransient<MainWindow>();
+}
