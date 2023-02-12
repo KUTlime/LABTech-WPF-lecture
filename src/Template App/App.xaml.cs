feb4026112df6a5e2bbacad3694bbf15093a69a1
Radek Zahradník <kutlime@gmail.com>
2023-02-12 20:20:02 +0100

    Add WPF project template

---
+++ src/Template App/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace TemplateApp;
+
+/// <summary>
+/// Main application class. It represents whole application.
+/// The Main method is generated behind the scenes.
+/// </summary>
+public partial class App : Application
+{
+    private readonly ServiceProvider _serviceProvider;
+
+    public App()
+    {
+        ServiceCollection services = new();
+        _serviceProvider = services.ConfigureServices().BuildServiceProvider();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mainWindow = _serviceProvider.GetService<MainWindow>();
+        mainWindow?.Show();
+    }
+}
---
+++ src/Template App/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TemplateApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml.
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+}
